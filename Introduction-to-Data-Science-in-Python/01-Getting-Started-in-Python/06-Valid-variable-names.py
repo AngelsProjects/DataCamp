a673308ab275097df1c659771fdac559c6f104f9
Angel Arciniega <mangel.arciniega@gmail.com>
2020-04-20 15:16:45 -0700

    Add "Introduction-to-Data-Science-in-Python" Course

---
+++ Introduction-to-Data-Science-in-Python/01-Getting-Started-in-Python/06-Valid-variable-names.py
@@ -0,0 +1,16 @@
+'''
+
+Valid variable names
+Which of the following is not a valid variable name?
+
+Instructions
+50 XP
+Possible Answers
+1 - my_dog_bayes
+2 - BAYES42
+3 - 3dogs
+4 - this_is_a_very_long_variable_name_42
+
+Answer: 3
+
+'''
